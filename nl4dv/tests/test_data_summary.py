50527e39a70beb674d3258c3815ad32006a3bea6
Arpit Narechania <arpitnarechania@gatech.edu>
2020-05-17 07:47:47 -0400

    integrate unit tests for data_genie (attribute datatype check) and queries (checking if NO VIS outputed for now); ToDo: validate attributes, tasks, and vis for queries

--- nl4dv/tests/test_data_summary.py
+++ nl4dv/tests/test_data_summary.py
@@ -4,29 +4,10 @@ import pytest
 import os
 
 
-# Written using Stanford Dependency Parser considering the JARs are at the below paths.
-nl4dv_instance = NL4DV(dependency_parser="stanford",
-                           path_to_models_jar=os.path.join("examples","assets","jars","stanford-english-corenlp-2018-10-05-models.jar"),
-                           path_to_parser_jar=os.path.join("examples","assets","jars","stanford-parser.jar"))
+class TestDataGenieClass:
 
-@pytest.fixture(scope='class')
-def data_summary(request):
-    dataset = request.param
-    return nl4dv_instance.set_data(data_url=os.path.join("examples","assets","data",dataset + ".csv"),
-                            alias_url=os.path.join("examples","assets","aliases",dataset + ".json"))
-
-
-@pytest.mark.parametrize("data_summary", ["euro"], indirect=True, scope="class")
-class TestSoccerDataSummaryClass:
-
-    def test_row_count(self, data_summary):
-        assert data_summary["rowCount"] == 552
-
-    def test_column_count(self, data_summary):
-        assert data_summary["columnCount"] == 8
-
-    def test_column_types(self, data_summary):
-        columns = {
+    dataset_attribute_type_mapping = {
+        'euro': {
             "Foot": constants.attribute_types["NOMINAL"],
             "Name": constants.attribute_types["NOMINAL"],
             "Position": constants.attribute_types["NOMINAL"],
@@ -35,10 +16,97 @@ class TestSoccerDataSummaryClass:
             "Age": constants.attribute_types["QUANTITATIVE"],
             "Salary": constants.attribute_types["QUANTITATIVE"],
             "Goals": constants.attribute_types["QUANTITATIVE"]
+        },
+        'colleges': {
+            "ACT Median": constants.attribute_types["QUANTITATIVE"],
+            "Admission Rate": constants.attribute_types["QUANTITATIVE"],
+            "Average Cost": constants.attribute_types["QUANTITATIVE"],
+            "Average Faculty Salary": constants.attribute_types["QUANTITATIVE"],
+            "Control": constants.attribute_types["NOMINAL"],
+            "Expenditure": constants.attribute_types["QUANTITATIVE"],
+            "Locale": constants.attribute_types["NOMINAL"],
+            "Median Debt": constants.attribute_types["QUANTITATIVE"],
+            "Median Earnings": constants.attribute_types["QUANTITATIVE"],
+            "Median Family Income": constants.attribute_types["QUANTITATIVE"],
+            "Name": constants.attribute_types["NOMINAL"],
+            "Population": constants.attribute_types["QUANTITATIVE"],
+            "Region": constants.attribute_types["NOMINAL"],
+            "SAT Average": constants.attribute_types["QUANTITATIVE"]
+        },
+        'movies-w-year': {
+            "Content Rating": constants.attribute_types["NOMINAL"],
+            "Creative Type": constants.attribute_types["NOMINAL"],
+            "Genre": constants.attribute_types["NOMINAL"],
+            "IMDB Rating": constants.attribute_types["QUANTITATIVE"],
+            "Production Budget": constants.attribute_types["QUANTITATIVE"],
+            "Release Year": constants.attribute_types["QUANTITATIVE"],
+            "Rotten Tomatoes Rating": constants.attribute_types["QUANTITATIVE"],
+            "Running Time": constants.attribute_types["QUANTITATIVE"],
+            "Title": constants.attribute_types["NOMINAL"],
+            "Worldwide Gross": constants.attribute_types["QUANTITATIVE"]
+        },
+        'housing': {
+            "Basement Area": constants.attribute_types["QUANTITATIVE"],
+            "Central Air": constants.attribute_types["NOMINAL"],
+            "Fence Type": constants.attribute_types["NOMINAL"],
+            "Fireplaces": constants.attribute_types["ORDINAL"],
+            "Foundation Type": constants.attribute_types["NOMINAL"],
+            "Garage Type": constants.attribute_types["NOMINAL"],
+            "Heating Type": constants.attribute_types["NOMINAL"],
+            "Home Type": constants.attribute_types["NOMINAL"],
+            "Lot Area": constants.attribute_types["QUANTITATIVE"],
+            "Lot Config": constants.attribute_types["NOMINAL"],
+            "Price": constants.attribute_types["QUANTITATIVE"],
+            "Roof Style": constants.attribute_types["NOMINAL"],
+            "Rooms": constants.attribute_types["ORDINAL"],
+            "Satisfaction": constants.attribute_types["ORDINAL"],
+            "Year": constants.attribute_types["ORDINAL"]
+        },
+        'olympic-medals': {
+            "Age": constants.attribute_types["QUANTITATIVE"],
+            "Bronze Medal": constants.attribute_types["QUANTITATIVE"],
+            "Country": constants.attribute_types["NOMINAL"],
+            "Gold Medal": constants.attribute_types["QUANTITATIVE"],
+            "Name": constants.attribute_types["NOMINAL"],
+            "Sex": constants.attribute_types["NOMINAL"],
+            "Silver Medal": constants.attribute_types["QUANTITATIVE"],
+            "Sport": constants.attribute_types["NOMINAL"],
+            "Total Medal": constants.attribute_types["QUANTITATIVE"],
+            "Year": constants.attribute_types["QUANTITATIVE"]
+        },
+        'cars-w-year': {
+            "Acceleration": constants.attribute_types["QUANTITATIVE"],
+            "Cylinders": constants.attribute_types["ORDINAL"],
+            "Displacement": constants.attribute_types["QUANTITATIVE"],
+            "Horsepower": constants.attribute_types["QUANTITATIVE"],
+            "MPG": constants.attribute_types["QUANTITATIVE"],
+            "Model": constants.attribute_types["NOMINAL"],
+            "Origin": constants.attribute_types["NOMINAL"],
+            "Weight": constants.attribute_types["QUANTITATIVE"],
+            "Year": constants.attribute_types["QUANTITATIVE"]
         }
+    }
+
+    @staticmethod
+    @pytest.fixture(scope='class')
+    def init_nl4dv(request):
+        dataset = request.param
+        dependency_parser_config = {'name': 'stanford','model': os.path.join("examples","assets","jars","stanford-english-corenlp-2018-10-05-models.jar"),'parser': os.path.join("examples","assets","jars","stanford-parser.jar")}
+        nl4dv_instance = NL4DV(verbose=False)
+        nl4dv_instance.set_dependency_parser(config=dependency_parser_config)
+        nl4dv_instance.set_data(data_url=os.path.join("examples","assets","data",dataset + ".csv"))
+        nl4dv_instance.set_alias_map(alias_url=os.path.join("examples","assets","aliases",dataset + ".json"))
+        return dataset, nl4dv_instance
+
+    @pytest.mark.parametrize("init_nl4dv", list(dataset_attribute_type_mapping.keys()), indirect=True, scope="class")
+    def test_attribute_datatypes(self, init_nl4dv):
+        dataset = init_nl4dv[0]
+        nl4dv_instance = init_nl4dv[1]
+        data_summary = nl4dv_instance.get_metadata()
+
         errors = []
-        for attr in data_summary["summary"]:
-            if data_summary["summary"][attr]["dataType"] != columns[attr]:
+        for attr in data_summary:
+            if data_summary[attr]["dataType"] != TestDataGenieClass.dataset_attribute_type_mapping[dataset][attr]:
                 errors.append(1)
 
         assert len(errors) == 0
--- nl4dv/tests/test_queries.py
+++ nl4dv/tests/test_queries.py
@@ -1,52 +1,49 @@
-from nl4dv import *
-from nl4dv.utils import constants
+from nl4dv import NL4DV
+from nl4dv.utils import helpers
+import json
 import pytest
 import pandas as pd
 import os
 
-nl4dv_instance = NL4DV(dependency_parser="stanford",
-                           path_to_models_jar=os.path.join("examples","assets","jars","stanford-english-corenlp-2018-10-05-models.jar"),
-                           path_to_parser_jar=os.path.join("examples","assets","jars","stanford-parser.jar"))
 
+class TestQueriesClass:
 
-filename = os.path.join('nl4dv','tests','queries.xlsx')
-@pytest.fixture(scope='class')
-def read_queries(request):
-    dataset = request.param
-    queries = pd.read_excel(filename, sheet_name=dataset, index_col=1)
-    nl4dv_instance.set_data(data_url=os.path.join("examples","assets","data",dataset + ".csv"),
-                            alias_url=os.path.join("examples","assets","aliases",dataset + ".json"))
-    return queries
+    _files = ['1-fullyspecified-attributes-tasks-vis.txt',
+              '2-underspecified-attributes-tasks.txt',
+              '3-underspecified-attributes-vis.txt',
+              '4-underspecified-attributes.txt',
+              'debugger_queries.txt',
+              'vis_matrix_queries.tsv'
+              ]
 
+    @staticmethod
+    def init_nl4dv(dataset, alias_file):
+        # Base NL4DV instance
+        dependency_parser_config = {'name': 'stanford','model': os.path.join("examples","assets","jars","stanford-english-corenlp-2018-10-05-models.jar"),'parser': os.path.join("examples","assets","jars","stanford-parser.jar")}
+        nl4dv_instance = NL4DV(verbose=False)
+        nl4dv_instance.set_dependency_parser(config=dependency_parser_config)
+        nl4dv_instance.set_data(data_url=os.path.join("examples","assets","data",dataset))
+        nl4dv_instance.set_alias_map(alias_url=os.path.join("examples","assets","aliases", alias_file))
+        return nl4dv_instance
 
-@pytest.mark.parametrize("read_queries", ["euro"], indirect=True, scope="class")
-class TestSoccerQueriesClass:
-    def test_row_count(self, read_queries):
-        errors = []
-        for query_index, row in read_queries.iterrows():
-            output = json.loads(json.dumps(nl4dv_instance.analyze_query(str(query_index)), default=helpers.set_default))
-            if set(output["attributeMap"].keys()) != set(row["attributes"].split(",")):
-                errors.append(1)
-        assert len(errors) == 0
+    @pytest.fixture(scope='class')
+    def read_queries(self, request):
+        query_file = os.path.join('examples', 'assets', 'queries', request.param)
+        return pd.read_csv(query_file, sep='\t')
 
+    @pytest.mark.parametrize("read_queries", _files, indirect=True, scope="class")
+    def test_queries(self, read_queries):
+        nl4dv_instance, dataset, errors = None, None, list()
+        for index, row in read_queries.iterrows():
+            if dataset != row['Datafile']:
+                dataset = row['Datafile']
+                alias_file = row['Aliasfile']
+                nl4dv_instance = TestQueriesClass.init_nl4dv(dataset, alias_file)
 
-@pytest.mark.parametrize("read_queries", ["cereals"], indirect=True, scope="class")
-class TestCerealsQueriesClass:
-    def test_row_count(self, read_queries):
-        errors = []
-        for query_index, row in read_queries.iterrows():
-            output = json.loads(json.dumps(nl4dv_instance.analyze_query(str(query_index)), default=helpers.set_default))
-            if set(output["attributeMap"].keys()) != set(row["attributes"].split(",")):
-                errors.append(1)
-        assert len(errors) == 0
-
-
-@pytest.mark.parametrize("read_queries", ["cars"], indirect=True, scope="class")
-class TestCarsQueriesClass:
-    def test_row_count(self, read_queries):
-        errors = []
-        for query_index, row in read_queries.iterrows():
-            output = json.loads(json.dumps(nl4dv_instance.analyze_query(str(query_index)), default=helpers.set_default))
-            if set(output["attributeMap"].keys()) != set(row["attributes"].split(",")):
-                errors.append(1)
+            if nl4dv_instance is not None:
+                output = json.loads(json.dumps(nl4dv_instance.analyze_query(row['Query']), default=helpers.set_default))
+                if len(output["visList"]) == 0:
+                    errors.append(1)
+                # if set(output["attributeMap"].keys()) != set(row["attributes"].split(",")):
+                #     errors.append(1)
         assert len(errors) == 0
