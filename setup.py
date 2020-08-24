65a8ccfd5fcc05681571e15e99685c181a34ff29
Arpit Narechania <arpitnarechania@gatech.edu>
2020-08-24 19:32:45 -0400

    bump up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nl4dv',
-    version='0.0.9',
+    version='0.1.0',
     author='Arpit Narechania',
     author_email='arpitnarechania@gatech.edu',
     packages=find_packages(),
