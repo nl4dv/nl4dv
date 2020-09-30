8f6102df1689d954108b90cb5ae7d141bc82cd76
Arpit Narechania <arpitnarechania@gatech.edu>
2020-09-30 10:29:15 -0400

    remove unwanted/unused dependencies

--- setup.py
+++ setup.py
@@ -15,20 +15,15 @@ setup(
     description='Natural Language Toolkit for Data Visualization',
     long_description=open('README.txt').read(),
     install_requires=[
-        "nltk==3.4.5",
-        "spacy==2.2",
-        "python-dateutil==2.6.1",
+        "dateparser>=0.7.6",
         "fuzzywuzzy==0.8.1",
+        "nltk>=3.5",
         "pytest==3.10.1",
         "pytest-cov==2.6.0",
+        "python-dateutil==2.6.1",
         "python-Levenshtein==0.12.0",
-        "pandas==1.0.2",
-        "numpy==1.16.6",
-        "xlrd==1.2.0",
+        "si-prefix==1.2.2",
+        "spacy>=2.2",
         "vega==3.1",
-        "ipython==7.12.0",
-        "Flask==1.0.0",
-        "Flask-Compress==1.3.0",
-        "si-prefix==1.2.2"
     ]
 )
