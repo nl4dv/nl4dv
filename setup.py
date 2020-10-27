cb895380cbcad74401b95874416269970dc0823d
Arpit Narechania <arpitnarechania@gatech.edu>
2020-10-27 01:07:24 -0400

    bump up version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='nl4dv',
-    version='0.0.1',
+    version='0.0.2',
     author='Arpit Narechania',
     author_email='arpitnarechania@gatech.edu',
     packages=find_packages(),
