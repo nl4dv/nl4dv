365cf65df102770dc5761e311a260085ecd0f4ce
Arpit Narechania <arpitnarechania@gmail.com>
2025-07-01 00:40:21 +0800

    bump up minor version after merging remote

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='nl4dv',
-    version='4.0.0',
+    version='4.1.0',
     author='Arpit Narechania',
     author_email='arpitnarechania@gmail.com',
     packages=find_packages(),
